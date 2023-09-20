0348228a362a964ff6eb5295d860924d41833aaf
Nimesh Viduranga Bandara <122119249+nisala1997@users.noreply.github.com>
2023-09-20 17:57:20 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import './app.css'
+import Navbar from "./Components/Navbar/Navbar"
+import Home from "./Components/Home/Home"
+import Main from "./Components/Main/Main"
+import Footer from "./Components/Footer/Footer"
 
-function App() {
-  return (
-   <>
-   
-   </>
-  );
+
+const App = () => {
+    return(
+        <>
+        <Navbar/>
+        <Home/>
+        <Main/>
+        <Footer/>
+        </>
+    )
 }
 
-export default App;
+export default App
