f48178a17212d2ac5926d37d034197240bca060e
Nimesh Viduranga Bandara <122119249+nisala1997@users.noreply.github.com>
2023-09-23 23:43:38 +0530

    api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react"
 import './app.css'
 
+// import search from './assets/icons/search.svg'
+// import { useStateContext } from '../src/Context'
+// import { BackgroundLayout, WeatherCard, MiniCard } from './Components/MainWeather'
+
 import NavbarAfterLogin  from "./Components/Navbar After Login/Navbar"
 import NavbarBeforeLogin from "./Components/Navbar Before Login/Navbar"
 import NavbarMarket from "./Components/Market Navbar/Navbar"
@@ -25,5 +29,74 @@ const App = () => {
         </>
     )
 }
+
+// function App() {
+
+//     const [input, setInput] = useState('')
+//     const { weather, thisLocation, values, place, setPlace } = useStateContext()
+//     // console.log(weather)
+  
+//     const submitCity = () => {
+//       setPlace(input)
+//       setInput('')
+//     }
+  
+//     return (
+//       <>
+//         <Navbar/>
+//         <div className='w-full h-screen text-white px-8'>
+//         <br></br>
+//         <br></br>
+//         <br></br>
+//         <nav className='w-full p-3 flex justify-between items-center'>
+//           <h1 className='font-bold tracking-wide text-3xl'>Weather</h1>
+//           <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
+//             <img src={search} alt="search" className='w-[1.5rem] h-[1.5rem]' />
+//             <input onKeyUp={(e) => {
+//               if (e.key === 'Enter') {
+//                 // sumit the form
+//                 submitCity()
+//               }
+//             }} type="text" placeholder='Search city' className='focus:outline-none w-full text-[#212121] text-lg' value={input} onChange={e => setInput(e.target.value)} />
+//           </div>
+//         </nav>
+//         <BackgroundLayout></BackgroundLayout>
+//         <main className='w-full flex flex-wrap gap-8 py-4 px-[10%] items-center justify-center'>
+//           <WeatherCard
+//             place={thisLocation}
+//             windspeed={weather.wspd}
+//             humidity={weather.humidity}
+//             temperature={weather.temp}
+//             heatIndex={weather.heatindex}
+//             iconString={weather.conditions}
+//             conditions={weather.conditions}
+//           />
+  
+//           <div className='flex justify-center gap-8 flex-wrap w-[60%]'>
+//             {
+//               values?.slice(1, 7).map(curr => {
+//                 return (
+//                   <MiniCard
+//                     key={curr.datetime}
+//                     time={curr.datetime}
+//                     temp={curr.temp}
+//                     iconString={curr.conditions}
+//                   />
+//                 )
+//               })
+//             }
+//           </div>
+//         </main>
+//         <br></br>
+//         <br></br>
+//         <br></br>
+//         <br></br>
+//         <br></br>
+//         <br></br>
+//         <Footer/>
+//         </div>
+//       </>
+//     )
+//   }
 
 export default App
