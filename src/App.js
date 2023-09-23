c944e8b3d95ca91ab445a5d7cfe23be56680cb90
Nimesh Viduranga Bandara <122119249+nisala1997@users.noreply.github.com>
2023-09-24 00:30:27 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,31 @@
+//import React from "react"
+import './app.css'
+
+import NavbarAfterLogin  from "./Components/Navbar After Login/Navbar"
+//import NavbarBeforeLogin from "./Components/Navbar Before Login/Navbar"
+//import NavbarMarket from "./Components/Market Navbar/Navbar"
+
+import HomeMarket from "./Components/Market Home/Home"
+//import HomeInformation from "./Components/Information Home/Home"
+//import HomeSupport from "./Components/Support Home/Home"
+
+import MainMarket from "./Components/Market Main/Main"
+//import MainInformation from "./Components/Information Main/Main"
+//import MainSupport from "./Components/Support Main/Main"
+//import MainWeather from "./Components/MainWeather/BackgroundLayout"
+
+import Footer from "./Components/Footer/Footer"
+
+const App = () => {
+    return(
+        <>
+        <NavbarAfterLogin/>
+        <HomeMarket/>
+        <MainMarket/>
+        <Footer/>
+        </>
+    )
+}
+
+
+export default App
