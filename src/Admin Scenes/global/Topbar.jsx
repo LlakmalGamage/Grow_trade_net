26b1a3014206f65d71c872dc454317fb8c8a3d95
llakmalgamage@gmail.com <llakmalgamage@gmail.com>
2023-10-16 23:41:06 +0530

    commit after admin

---
+++ src/Admin Scenes/global/Topbar.jsx
@@ -0,0 +1,60 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+// import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import {HiOutlineInboxArrowDown} from 'react-icons/hi2'
+import {FcBusinessman} from 'react-icons/fc'
+import SearchIcon from "@mui/icons-material/Search";
+import {RiPlantFill} from 'react-icons/ri'
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <><div className='logoDiv'>
+      <a href='#link' className='logo flex'>
+        <h1><RiPlantFill className='icon' /> GROW TRADE NET</h1>
+      </a>
+    </div><Box display="flex" justifyContent="space-between" p={2}>
+        {/* SEARCH BAR */}
+        <Box
+          display="flex"
+          backgroundColor={colors.primary[400]}
+          borderRadius="3px"
+        >
+          <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+          <IconButton type="button" sx={{ p: 1 }}>
+            <SearchIcon />
+          </IconButton>
+        </Box>
+
+        {/* ICONS */}
+        <Box display="flex">
+          <IconButton onClick={colorMode.toggleColorMode}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlinedIcon />
+            ) : (
+              <LightModeOutlinedIcon />
+            )}
+          </IconButton>
+          <IconButton>
+            <HiOutlineInboxArrowDown />
+          </IconButton>
+          {/* <IconButton>
+            <SettingsOutlinedIcon />
+          </IconButton> */}
+          <IconButton>
+            <FcBusinessman />
+          </IconButton>
+        </Box>
+      </Box></>
+  );
+};
+
+export default Topbar;
