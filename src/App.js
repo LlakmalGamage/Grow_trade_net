1741938e14938249bb4f79e033c67a16678b38f5
llakmalgamage@gmail.com <llakmalgamage@gmail.com>
2023-10-18 14:05:56 +0530

    1st successful admin test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react"
-import './App.css'
+// import React from "react"
+// import './App.css'
 
-
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
 
 function App() {
-  // const [theme, colorMode] = useMode();
-  // const [isSidebar, setIsSidebar] = useState(true);
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (    
     <>
@@ -45,7 +61,7 @@ function App() {
       </ThemeProvider>
     </ColorModeContext.Provider> 
     </>
-  )
+  );
 }
 
 export default App;
