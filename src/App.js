a0b20406d4d8ff9971aa2a2220b2fa96db1e810f
Nimesh Viduranga Bandara <122119249+nisala1997@users.noreply.github.com>
2023-10-21 19:10:38 +0530

    ?

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import { useState } from "react";
 import { Routes, Route } from 'react-router-dom'; 
 // import Topbar from "./scenes/global/Topbar";
@@ -26,18 +27,43 @@ import FarmerHomePage from './Pages/FarmerHomePage/Home'
 // import ExportersHomePage from './Pages/ExportersHomePage/Home'
 // import ExportersMylistPage from './Pages/ExportersMyListPage/MyList'
 // import ExportersSupportPage from './Pages/ExportersSupportPage/Support'
+=======
+// import React from "react"
+// import './App.css'
+>>>>>>> afa40fceb78accbf789803658a4bbae2ee59e561
 
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
 
 function App() {
-  // const [theme, colorMode] = useMode();
-  // const [isSidebar, setIsSidebar] = useState(true);
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
-  return (
+  return (    
     <>
+<<<<<<< HEAD
       <Routes>
         
         {/* FarmerPage */}
         <Route path='/' element={<FarmerHomePage/>}/>
+=======
+      {/* <Routes>
+>>>>>>> afa40fceb78accbf789803658a4bbae2ee59e561
         <Route path='/home' element={<FarmerHomePage/>}/>
         <Route path='/market' element={<MarketPage/>}/>
         <Route path='/information' element={<InformationPage/>}/>
@@ -47,14 +73,44 @@ function App() {
         {/* <Route path='/' element={<ExportersHomePage/>}/>
         <Route path='/home' element={<ExportersHomePage/>}/>
         <Route path='/mylist' element={<ExportersMylistPage/>}/>
-        <Route path='/support' element={<ExportersSupportPage/>}/>
+        <Route path='/support' element={<ExportersSupportPage/>}/> */}
 
         
+<<<<<<< HEAD
+      </Routes>
+=======
 
         
         
       {/* </Routes> } */}
 
+>>>>>>> afa40fceb78accbf789803658a4bbae2ee59e561
+
+
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider> 
     </>
   );
 }
