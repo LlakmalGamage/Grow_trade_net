ca857eb99e134f4c9278988f505b85f487047a2e
Nimesh Viduranga Bandara <122119249+nisala1997@users.noreply.github.com>
2023-12-01 18:23:45 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,87 +1,17 @@
-// import { Routes, Route } from 'react-router-dom'; 
 
-// import MarketPage from '../src/Pages/MarketPage/Market'
-// import SupportPage_F from '../src/Pages/SupportPage/Support'
-// import FarmerHomePage from './Pages/FarmerHomePage/Home'
-// import WeatherPage from './Pages/WeatherPage/weather'
 
-// import ExportersHomePage from './Pages/ExportersHomePage/Home'
-// import ExportersMylistPage from './Pages/ExportersMyListPage/MyList'
-// import ExportersSupportPage from './Pages/ExportersSupportPage/Support'
 
-// import MainPage from './Pages/MainPage/main'
-// import DashBoardPage from './Pages/AdminDashboard/dashboard'
-// import WholesalerPage from './Pages/WholesalerPage/wholesaler'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// import Dashboard from "./scenes/dashboard";
-// import Add from "./scenes/add";
-// import Invoices from "./scenes/invoices";
-// import User from "./scenes/user";
-// import Bar from "./scenes/bar";
-// import Profile from "./scenes/profile";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar/calendar";
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import { AuthProvider } from '../src/Context/AuthContex'
-import Header from '../src/Components/Header'
-import HomePage from '../src/Pages/HomePage'
-import LoginPage from '../src/Pages/LoginPage'
-import RegisterPage from '../src/Pages/RegisterPage'
-import Dashboard from '../src/Pages/Dashboard'
-import ProtectedRoutes from '../src/Utils/ProtectedRoutes';
+// import { AuthProvider } from '../src/Context/AuthContex'
+// import Header from '../src/Components/Header'
+// import HomePage from '../src/Pages/HomePage'
+// import LoginPage from '../src/Pages/LoginPage'
+// import RegisterPage from '../src/Pages/RegisterPage'
+// import Dashboard from '../src/Pages/Dashboard'
+// import ProtectedRoutes from '../src/Utils/ProtectedRoutes';
 
 {/*
-function App() {
-  return (    
-    <>
-      <Routes>
-        <Route path='/' element={<MainPage/>}/>
-        <Route path='/farmers' element={<FarmerHomePage/>}/>
-
-          <Route path='/' element={<FarmerHomePage/>}/>
-          <Route path='/home' element={<FarmerHomePage/>}/>
-          <Route path='/market' element={<MarketPage/>}/>
-          <Route path='/support_f' element={<SupportPage_F/>}/>
-          <Route path='/weather' element={<WeatherPage/>}/>
-
-          <Route path='/exporters' element={<ExportersHomePage/>}/>
-
-          <Route path='/' element={<ExportersHomePage/>}/>
-          <Route path='/homeex' element={<ExportersHomePage/>}/>
-          <Route path='/mylist' element={<ExportersMylistPage/>}/>
-          <Route path='/support_e' element={<ExportersSupportPage/>}/>
-
-
-          <Route path='/dashboard' element={<DashBoardPage/>}/>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/add" element={<Add/>} />
-                <Route path="/user" element={<User />} />
-                <Route path="/invoices" element={<Invoices />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/bar" element={<Bar />} />
-                <Route path="/pie" element={<Pie />} />
-                <Route path="/line" element={<Line />} />
-                <Route path="/faq" element={<FAQ />} />
-                <Route path="/calendar" element={<Calendar/>} />
-                <Route path="/geography" element={<Geography />} />
-
-        <Route path='/whole' element={<WholesalerPage/>}/>
- 
-      </Routes> 
-  
-    </>
-  );
-}
-
-export default App;
-*/}
-
-
 function App() {
   return (
     <BrowserRouter>
@@ -98,6 +28,98 @@ function App() {
         </Routes>
       </AuthProvider>
     </BrowserRouter>
+  );
+}
+
+export default App;
+*/}
+
+// import { useState } from "react";
+import { Routes, Route } from 'react-router-dom'; 
+// import './app.css'
+
+import MarketPage from '../src/Pages/MarketPage/Market'
+import SupportPage_F from '../src/Pages/SupportPage/Support'
+import FarmerHomePage from './Pages/FarmerHomePage/Home'
+import WeatherPage from './Pages/WeatherPage/weather'
+
+import ExportersHomePage from './Pages/ExportersHomePage/Home'
+import ExportersMylistPage from './Pages/ExportersMyListPage/MyList'
+import ExportersSupportPage from './Pages/ExportersSupportPage/Support'
+
+import MainPage from './Pages/MainPage/main'
+import DashBoardPage from './Pages/AdminDashboard/dashboard'
+// import WholesalerPage from './Pages/WholesalerPage/wholesaler'
+import WholesalerPage from './Pages/WholesalerPage/wholesaler'
+
+
+import Dashboard from "./scenes/dashboard";
+import Add from "./scenes/add";
+import Invoices from "./scenes/invoices";
+import User from "./scenes/user";
+import Bar from "./scenes/bar";
+import Profile from "./scenes/profile";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import Calendar from "./scenes/calendar/calendar";
+
+function App() {
+  return (    
+    <>
+      <Routes>
+        <Route path='/' element={<MainPage/>}/>
+        <Route path='/farmers' element={<FarmerHomePage/>}/>
+        
+        {/* FarmerPage */}
+          <Route path='/' element={<FarmerHomePage/>}/>
+          <Route path='/home' element={<FarmerHomePage/>}/>
+          <Route path='/market' element={<MarketPage/>}/>
+          <Route path='/support_f' element={<SupportPage_F/>}/>
+          <Route path='/weather' element={<WeatherPage/>}/>
+
+          <Route path='/exporters' element={<ExportersHomePage/>}/>
+      
+
+        {/* Exporter Page */}
+          <Route path='/' element={<ExportersHomePage/>}/>
+          <Route path='/homeex' element={<ExportersHomePage/>}/>
+          <Route path='/mylist' element={<ExportersMylistPage/>}/>
+          <Route path='/supportex' element={<ExportersSupportPage/>}/>
+          <Route path='/support_e' element={<ExportersSupportPage/>}/>
+
+        {/* Dashboard Page */} 
+        
+          <Route path='/dashboard' element={<DashBoardPage/>}/>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/add" element={<Add/>} />
+                <Route path="/user" element={<User />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/calendar" element={<Calendar/>} />
+                <Route path="/geography" element={<Geography />} />
+                <Route path="/Market" element={<MarketPage />} />
+
+        {/* Wholesaler Page */} 
+        {/* <Route path='/whole' element={<WholesalerPage/>}/> */}
+        {/* Wholesaler Page  */}
+        <Route path='/whole' element={<WholesalerPage/>}/>
+
+          
+          
+          
+
+        
+      </Routes>
+
+
+    
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { StateContextProvider } from './Context/index'
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StateContextProvider>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </React.StrictMode>
   </StateContextProvider>
 );
 
-root.render(
-  <React.StrictMode> {/* Wrap your entire application with <React.StrictMode> */}
-        <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode> {/* Wrap your entire application with <React.StrictMode> */}
+//         <App />
+//   </React.StrictMode>
+// );
