db37990ea9f4ba6fbe96dacf4f43cce66c8d0c30
Shalani Sooriarachchi <shalani.sooriarachchi@gmail.com>
2024-02-11 10:34:57 +0530

    Add files via upload

---
+++ src/Components/WholesalerMain/main.jsx
@@ -0,0 +1,35 @@
+import './main.css'
+import video1 from '../../Components/Audio And Video/v1.mp4'
+import video2 from '../../Components/Audio And Video/v5.mp4'
+import video3 from '../../Components/Audio And Video/v3.mp4'
+import video4 from '../../Components/Audio And Video/v4.mp4'
+// import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+const Main = () => {
+  return(
+    <div className='main'>
+      <h2 data-aos='fade-right'>AGRICULTURE SUPPORT HUB</h2>
+      <p data-aos='fade-right'>GROW TRADE NET</p>
+      <div className='first-des'>
+        <div className='image'>
+            <video src={video1} muted autoPlay loop type="warehouse/mp4"></video>
+                
+
+
+            <video src={video2} muted autoPlay loop type="warehouse/mp4"></video>
+        </div>
+
+        <div className='image'>
+            <video src={video3} muted autoPlay loop type="warehouse/mp4"></video>
+            <video src={video4} muted autoPlay loop type="warehouse/mp4"></video>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+
+export default Main
