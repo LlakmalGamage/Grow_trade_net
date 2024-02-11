194980e2120bd9586569a8b59344187666553530
Shalani Sooriarachchi <shalani.sooriarachchi@gmail.com>
2024-02-11 13:23:51 +0530

    Add files via upload

---
+++ src/Components/WholesalerAfterLoginNavbar/FarmerHome/Home.jsx
@@ -0,0 +1,61 @@
+import {useEffect} from 'react'
+import './home.css'
+import video from '../../Components/Audio And Video/cornfield.mp4'
+import {FiFacebook} from 'react-icons/fi'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {SiTripadvisor} from 'react-icons/si'
+import {BsListTask} from 'react-icons/bs'
+import {TbApps} from 'react-icons/tb'
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+const Home = () => {
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  },[])
+
+
+
+  return (
+    <section className="home">
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop type="cornfield/mp4"></video>
+      {/* <img src={image} alt="Description of the image" /> */}
+
+
+      <div className='homeContent container'>
+        <div className="textDiv">
+          <span data-aos='fade-up' className='smallText'>
+            
+          </span>
+          <h1 data-aos='fade-scale-up' className='homeTitle'>
+            HELLO FARMER&apos;S
+          </h1>
+        </div>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+
+        <div data-aos='fade-up' className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <FiFacebook className='icon'/>
+            <AiOutlineInstagram className='icon'/>
+            <SiTripadvisor className='icon'/>
+          </div>
+
+          <div className="leftIcons">
+            <BsListTask className='icon'/>
+            <TbApps className='icon'/>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+  )
+}
+
+export default Home
---
+++ src/Components/WholesalerAfterLoginNavbar/Navbar.jsx
@@ -0,0 +1,101 @@
+import {useState} from 'react'
+import './navbar.css'
+// import {MdOutlineTravelExplore} from 'react-icons/md'
+import {RiPlantFill} from 'react-icons/ri'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {TbGridDots} from 'react-icons/tb'
+import {HiOutlineInboxArrowDown} from 'react-icons/hi2'
+import {FcBusinessman} from 'react-icons/fc'
+// import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  const[active,setActive] = useState('navBar');
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  // Function to toggle navBar
+  const showNav = ()=>{
+    setActive('navBar activeNavbar')
+  }
+  // Function to remove navBar
+  const removeNavbar = ()=>{
+    setActive('navBar');
+    setDropdownVisible(false);
+  }
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  return (
+    <section className='navBarSection'>
+      <header className='header flex'>
+
+        <div className='logoDiv'>
+          <a href='/farmers/home' className='logo flex'>
+          <RiPlantFill className='icon'/>
+            <h1 className='logo-heading'> GROW TRADE NET</h1>
+            <h5 className='farmers-heading'>WHOLESALERS</h5>
+          </a>
+        </div>
+
+        <div className={active}>
+          <ul className='navLists flex'>
+            <li className='navItem'>
+              <a href='/farmers/home' className='navLink'>Home</a>
+            </li>
+            <li className='navItem'>
+              {/* <CustomLink to='/information' className='navLink'>Information</CustomLink> */}
+              {/* <a href='/information' className='navLink'>Information</a> */}
+            </li>
+            <li className='navItem'>
+              {/* <CustomLink to='/market' className='navLink'>Market</CustomLink> */}
+              <a href='/farmers/market' className='navLink'>Market</a>
+            </li>
+            <li className='navItem'>
+              <a href='/WholesalerListings' className='navLink'>Listings</a>
+            </li>
+            <li className='navItem'>
+              <a href='/farmers/support_f' className='navLink'>Support</a>
+            </li>
+            {/* <li className='navItem'>
+              <a href='#link' className='navLink'>Dashboard</a>
+            </li>
+            */}
+            <button className='btn'>
+            <a href="#link"><HiOutlineInboxArrowDown className='icon'/></a>
+            </button> 
+
+            {/* profile photo */}
+              <button className="btn" onClick={toggleDropdown}>
+                <FcBusinessman className="icon" />
+              </button>
+              {dropdownVisible && (
+                <div className="dropdownMenu">
+                  <ul>
+                    <li>
+                      <a href="/profile">Profile</a>
+                    </li>
+                    <li>
+                      <a href="/logout">Logout</a>
+                    </li>
+                  </ul>
+                </div>
+              )}
+              
+          </ul>
+
+          <div onClick={removeNavbar} className="closeNavbar">
+            <AiFillCloseCircle className="icon"/>
+          </div>
+        </div>
+
+        <div onClick={showNav} className='toggleNavbar'>
+          <TbGridDots className='icon'/>
+
+        </div>
+
+      </header>
+    </section>
+  )
+}
+
+
+export default Navbar
